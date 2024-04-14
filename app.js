7a4ae909d5406e2ae55d9dbd095a79c395b52f33
MuhammadAhmed2 <exact.ahmed@gmail.com>
2024-04-15 02:39:10 +0500

    header animation

--- app.js
+++ app.js
@@ -1,14 +1,7 @@
-var slideIndex = 0;
-carousel();
+const burgerIcon = document.getElementById('burger-icon');
+const modal = document.getElementById('modal');
 
-function carousel() {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > x.length) {slideIndex = 1}
-  x[slideIndex-1].style.display = "block";
-  setTimeout(carousel, 3000); // Change image every 2 seconds
-}
+
+burgerIcon.addEventListener('click', () => {
+    modal.classList.toggle('show');
+});
