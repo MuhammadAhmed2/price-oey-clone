19a2dd1d8fe0f63d8f89154a3b29c98ab938ab6d
MuhammadAhmed2 <exact.ahmed@gmail.com>
2024-04-15 10:35:13 +0500

    js part final

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
-const burgerIcon = document.getElementById('burger-icon');
-const modal = document.getElementById('modal');
+var slideIndex = 0;
+carousel();
 
-
-burgerIcon.addEventListener('click', () => {
-    modal.classList.toggle('show');
-});
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > x.length) {slideIndex = 1}
+  x[slideIndex-1].style.display = "block";
+  setTimeout(carousel, 3000); // Change image every 2 seconds
+}
